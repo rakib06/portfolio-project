164ed351a48788619af7c3c6aa76154fbfa7cf48
Rakib <six.rakib@gmail.com>
2019-12-09 04:14:29 +0600

    our first commit

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# create a blog models
+# title
+# publication_date
+# body
+# image
+class Blog(models.Model):
+    title = models.CharField(max_lengh=255)
+    publication_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+# add blog apps to the settings
+
+# create migration
+
+# migrate
+
+# add to the admin
