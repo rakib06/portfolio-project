30fa4344a4db6508e81a6dbd076b01300862b52f
Rakib <six.rakib@gmail.com>
2019-12-09 21:32:14 +0600

    learned mysql db

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -84,10 +85,6 @@ DATABASES = {
 
     }
 }
-
-
-'''
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -98,8 +95,19 @@ DATABASES = {
         'PORT': '5432',
 
     }
+} '''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfolio',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
+    }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
