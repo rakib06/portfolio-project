d806a4d1122178f5a5cb55ab895aca25a58cebae
Rakib <six.rakib@gmail.com>
2019-12-12 03:33:48 +0600

    Bootstrap with django

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,21 @@
 from django.db import models
 
 
-# create a blog models
-# title
-# publication_date
-# body
-# image
-'''
+# inheritance
 class Blog(models.Model):
-    title = models.CharField(max_lengh=255)
+    title = models.CharField(max_length=255)
     publication_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-'''
-# add blog apps to the settings
 
-# create migration
-
-# migrate
-
-# add to the admin
+# ------------Road Map-----------------
+# 1. create a blog models
+# title
+# publication_date
+# body
+# image
+# 2. add blog apps to the settings
+# 3. create migration
+# 4. migrate
+# 5. add to the admin
